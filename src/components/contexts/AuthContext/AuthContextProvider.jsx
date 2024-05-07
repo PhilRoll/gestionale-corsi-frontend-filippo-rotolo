70be443937dd7d8cedbe6535fbb455cbebb493e8
PhilRoll <filippo.rotolo95@gmail.com>
2024-05-07 23:40:13 +0200

    aggiornamento

--- src/components/contexts/AuthContext/AuthContextProvider.jsx
+++ src/components/contexts/AuthContext/AuthContextProvider.jsx
@@ -6,38 +6,15 @@ import React from "react";
 export function AuthContextProvider({ children }) {
   // Stato per gestire le informazioni sull'utente
   const [user, setUser] = useState({
-    firstName: "",
-    lastName: "",
+    nome: "",
+    cognome: "",
     email: "",
-    isLogged: false, // Inizialmente l'utente non è loggato
-    isAuthorized: false, // Inizialmente l'utente non ha autorizzazioni
+    isLogged: false,
+    ruolo: ""
   });
 
-  // Funzione per effettuare il login
-  const login = () => {
-    // Simulazione di una richiesta di login
-    // Dopo aver autenticato l'utente, impostiamo isLogged su true e isAuthorized a seconda del ruolo o dei permessi
-    setUser((prevState) => ({
-      ...prevState,
-      isLogged: true,
-      isAuthorized: true
-      // Esempio: potrebbe essere impostato a true se l'utente ha un certo ruolo o livello di autorizzazione
-    }));
-  };
-
-  // Funzione per effettuare il logout
-  const logout = () => {
-    // Simulazione di una richiesta di logout
-    // Dopo aver eseguito il logout, reimpostiamo isLogged su false e isAuthorized su false
-    setUser((prevState) => ({
-      ...prevState,
-      isLogged: false
-      
-    }));
-  };
-
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, setUser }}>
       {children}
     </AuthContext.Provider>
   );
